6a80bd7f88d82542edc0b73b4c737f18ac4b2467
Nigel Rahkola <Nigel.Rahkola@enernoc.com>
2016-01-19 21:00:20 -0800

    Reorg the tiles export

--- test/tilesTest.js
+++ test/tilesTest.js
@@ -1,11 +1,26 @@
 var assert = require('assert');
-var tiles = require('../tiles');
+var Tiles = require('../tiles');
 
 describe('tiles', function() {
 	describe('#init()', function () {
 		it('should return a stick with the specified size', function () {
-			var map = tiles(3);
+			var tiles = Object.create(Tiles);
+			console.log(tiles);
+			tiles.init(3);
+			console.log(tiles);
 			assert.equal(map.stick.length === 9, true);
+		});
+	});
+
+	describe('#getTile()', function () {
+		it('should return the tile 8 at location (1,2)', function () {
+			debugger;
+			var map = tiles(3);
+			map.stick = ['0','0','0','0','0','8','0','0','0'];
+			console.log("@@@", map);
+			var tile = map.getTile(1,2);
+			console.log("@@@", tile);
+			assert( tile === '8');
 		});
 	});
 });
--- tiles.js
+++ tiles.js
@@ -1,17 +1,22 @@
-module.exports = function (size) {
-	var stick = [];
-	for(var i = 0; i < size * size; i++) {
-		stick.push(0);
-	}
-	return {
-		stick: stick,
-		getTile: function getTile(x, y) {
-			var tileIdx = y * this.size + x;
-			return this.stick[tileIdx];
-		},
-		setTile: function getTile(x, y, val) {
-			var tileIdx = y * this.size + x;
-			return this.stick[tileIdx] = val;
+
+var tiles = {
+	stick: [],
+	size: 0,
+	init: function init(size) {
+		this.size = size;
+		this.stick = [];
+		for(var i = 0; i < size * size; i++) {
+			stick.push(0);
 		}
-	};
-}
+	},
+	getTile: function getTile(x, y) {
+		var tileIdx = y * this.size + x;
+		return this.stick[tileIdx];
+	},
+	setTile: function getTile(x, y, val) {
+		var tileIdx = y * this.size + x;
+		return this.stick[tileIdx] = val;
+	}
+};
+
+module.exports = tiles;
